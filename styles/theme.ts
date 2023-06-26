b11ff2c7d2076fb861711131ea9e6935d0c3afd9
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-06-26 17:51:10 +0300

    feature: add neutral colors

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,36 +1,46 @@
-import { breakpointTablet, breakpointDesktop } from "./vars";
+import { breakpointTablet, breakpointDesktop } from "./vars"
 
 export const colors = {
-    mainBg: '',
-    secondaryBg: '',
-    buttonBg: '',
-    mainText: '',
-    error: '#DC0C31',
-    link: '#0029FF'
+  mainBg: "",
+  secondaryBg: "",
+  buttonBg: "",
+  mainText: "",
+  error: "#DC0C31",
+  link: "#0029FF",
+  //   neutral colors
+  lightGray: "#DDDDDD",
+  mediumGray: "#CECECE",
+  darkGray: "#BFBFBF",
+  charcoal: "#B1B1B1",
+  dimGray: "#939393",
+  steelGray: "#585858",
+  darkSteelGray: "#464646",
+  darkSlateGray: "#353535",
+  jetBlack: "#232323",
+  highlightYellow: "#FFE37E",
 }
 
 export const fonts = {
-    body: {
-        fontFamily: "'Exo 2', sans-serif",
-        fontSize: { desktop: 16, tablet: 14, mobile: 12 },
-        lineHeight: { tablet: 1.4, mobile: 1.33 },
-        leterSpacing: { tablet: 0.25, mobile: 0.4 },
-        fontWeight: 400,
-    },
-    label: {},
-    title: {},
-    headline: {},
-    display: {}
+  body: {
+    fontFamily: "'Exo 2', sans-serif",
+    fontSize: { desktop: 16, tablet: 14, mobile: 12 },
+    lineHeight: { tablet: 1.4, mobile: 1.33 },
+    leterSpacing: { tablet: 0.25, mobile: 0.4 },
+    fontWeight: 400,
+  },
+  label: {},
+  title: {},
+  headline: {},
+  display: {},
 }
 
 export const media = {
-    tablet: `min-width: ${breakpointTablet}px`,
-    desktop: `min-width: ${breakpointDesktop}px`
+  tablet: `min-width: ${breakpointTablet}px`,
+  desktop: `min-width: ${breakpointDesktop}px`,
 }
 
-
 export const theme = {
-    colors,
-    media,
-    fonts
+  colors,
+  media,
+  fonts,
 }
