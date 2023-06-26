64d958ae76ca8ff4c6a4ac27f78fe82b2ccc39d3
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-26 20:57:17 +0300

    refactor: update added fonts to project

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import Container from "components/Container/Container";
 import Styling from "components/Styling/Styling";
 
 export const metadata = {
-    title: 'Feeda',
-    description: "Candidate's application",
+	title: "Feeda",
+	description: "Candidate's application",
 };
 
 export default function Home() {
-    return (
-        <main>
-            <Container>
-                <Styling />
-            </Container>
-        </main>
-    );
+	return (
+		<main>
+			<Container>
+				<Styling />
+			</Container>
+		</main>
+	);
 }
