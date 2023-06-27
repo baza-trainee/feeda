fe23e18ab483042be3e3794ce4a26c71fabbd638
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-27 23:30:35 +0300

    refactor: update added fonts in project

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,36 +1,33 @@
 import { breakpointTablet, breakpointDesktop } from "./vars";
 
 export const colors = {
-    mainBg: '',
-    secondaryBg: '',
-    buttonBg: '',
-    mainText: '',
-    error: '#DC0C31',
-    link: '#0029FF'
-}
+	mainBg: "",
+	secondaryBg: "",
+	buttonBg: "",
+	mainText: "",
+	error: "#DC0C31",
+	link: "#0029FF",
+};
 
 export const fonts = {
-    body: {
-        fontFamily: "'Exo 2', sans-serif",
-        fontSize: { desktop: 16, tablet: 14, mobile: 12 },
-        lineHeight: { tablet: 1.4, mobile: 1.33 },
-        leterSpacing: { tablet: 0.25, mobile: 0.4 },
-        fontWeight: 400,
-    },
-    label: {},
-    title: {},
-    headline: {},
-    display: {}
-}
+	body: {
+		fontSize: { desktop: 16, tablet: 14, mobile: 12 },
+		lineHeight: { tablet: 1.4, mobile: 1.33 },
+		leterSpacing: { tablet: 0.25, mobile: 0.4 },
+		fontWeight: 400,
+	},
+	label: {},
+	title: {},
+	headline: {},
+	display: {},
+};
 
 export const media = {
-    tablet: `min-width: ${breakpointTablet}px`,
-    desktop: `min-width: ${breakpointDesktop}px`
-}
-
+	tablet: `min-width: ${breakpointTablet}px`,
+	desktop: `min-width: ${breakpointDesktop}px`,
+};
 
 export const theme = {
-    colors,
-    media,
-    fonts
-}
+	colors,
+	media,
+};
