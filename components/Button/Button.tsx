8d99e85968325cbb15f75361f5e726555cda8385
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-28 19:31:49 +0300

    refactor: add onClickHandler

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,12 +6,17 @@ import { buttonStyle } from "./Button.styles";
 interface ButtonProps {
 	children: ReactNode | string;
 	isDisabled?: boolean;
+	func: () => void;
 }
 
-const Title = ({ children, isDisabled }: ButtonProps) => {
+const Title = ({ children, isDisabled, func }: ButtonProps) => {
+	const onClickHandler = () => {
+		func();
+	};
 	return (
 		<button
 			css={buttonStyle}
+			onClick={onClickHandler}
 			disabled={isDisabled}
 		>
 			{children}
