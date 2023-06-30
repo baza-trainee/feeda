82b47417eb933f83a17a7d494dab042ffceeffb5
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-06-30 13:44:52 +0300

    refactor: save

--- client/components/UserApplication/helpers/handleChange.ts
+++ client/components/UserApplication/helpers/handleChange.ts
@@ -1,43 +1,43 @@
-import { emailRegex, linkedRegex, phoneNumberRegex } from "./index";
+import { emailRegex, linkedRegex, phoneNumberRegex } from './index';
 
 export const handleNameChange = (fieldName, value, clearErrors) => {
-  if (value.length >= 3 && value.length <= 16) {
-    clearErrors(fieldName);
-  }
+	if (value.length >= 3 && value.length <= 16) {
+		clearErrors(fieldName);
+	}
 };
 
 export const handleSurnameChange = (fieldName, value, clearErrors) => {
-  if (value.length >= 5 && value.length <= 16) {
-    clearErrors(fieldName);
-  }
+	if (value.length >= 5 && value.length <= 16) {
+		clearErrors(fieldName);
+	}
 };
 
 export const onHandleStackChange = (fieldName, value, clearErrors) => {
-  if (value.length >= 2 && value.length <= 300) clearErrors(fieldName);
+	if (value.length >= 2 && value.length <= 300) clearErrors(fieldName);
 };
 
 export const onHandlePhoneChange = (fieldName, value, clearErrors) => {
-  // console.log(phoneNumberRegex.test(value));
-  if (phoneNumberRegex.test(value)) {
-    clearErrors(fieldName);
-  }
+	// console.log(phoneNumberRegex.test(value));
+	if (phoneNumberRegex.test(value)) {
+		clearErrors(fieldName);
+	}
 };
 
 export const onHandleEmailChange = (fieldName, value, clearErrors) => {
-  if (emailRegex.test(value)) {
-    clearErrors(fieldName);
-  }
+	if (emailRegex.test(value)) {
+		clearErrors(fieldName);
+	}
 };
 
 // видаляємо рендер помилки поля 'LinkedIn'
 export const onHandleLinkedChange = (fieldName, value, clearErrors) => {
-  if (linkedRegex.test(value)) {
-    clearErrors(fieldName);
-  }
+	if (linkedRegex.test(value)) {
+		clearErrors(fieldName);
+	}
 };
 // видаляємо рендер помилки поля 'City'
 export const onHandleCityChange = (fieldName, value, clearErrors) => {
-  if (value.length >= 1 && value.length <= 22) {
-    clearErrors(fieldName);
-  }
+	if (value.length >= 1 && value.length <= 22) {
+		clearErrors(fieldName);
+	}
 };
