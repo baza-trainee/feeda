0eb7482fb5b2fd3d84697bfd19f8f0ec5b8483c7
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-07-14 14:09:27 +0300

    fix: corrected styles according to changes in the figma

--- client/components/EndPopUp/EndPopUp.styles.ts
+++ client/components/EndPopUp/EndPopUp.styles.ts
@@ -2,35 +2,31 @@ import styled from '@emotion/styled';
 import { theme } from 'styles/theme';
 const { colors, fonts, media } = theme;
 
-interface ExistingUserProp {
-	existingUser?: boolean;
-}
-
-export const Section = styled.section<ExistingUserProp>`
+export const Section = styled.section`
 	position: fixed;
 	left: 50%;
 	top: 50%;
 	transform: translate(-50%, -50%);
 	text-align: center;
 	width: 100%;
-	max-width: 286px;
+	max-width: 335px;
 	margin: 0 auto;
 	padding: 24px;
 	border-radius: 16px;
 	border: 1px solid ${colors.disabledBtnBg};
 	@media screen and (${media.tablet}) {
-		max-width: 394px;
+		max-width: 466px;
 		font-size: ${fonts.display.fontSize.mobile}px;
 		line-height: ${fonts.display.lineHeight.mobile};
 	}
 	@media screen and (${media.desktop}) {
-		max-width: ${(p) => (p.existingUser ? '912px' : '464px')};
+		max-width: 596px;
 		font-size: ${fonts.display.fontSize.tablet}px;
 		line-height: ${fonts.display.lineHeight.tablet};
 	}
 `;
 
-export const Title = styled.h1<ExistingUserProp>`
+export const Title = styled.h1`
 	color: ${colors.mainTitle};
 	margin-bottom: 32px;
 	font-weight: ${fonts.headline.fontWeight.tablet};
@@ -45,27 +41,29 @@ export const Title = styled.h1<ExistingUserProp>`
 		margin-bottom: 32px;
 		font-size: ${fonts.display.fontSize.tablet}px;
 		line-height: ${fonts.display.lineHeight.tablet};
-		white-space: ${(p) => (p.existingUser ? 'nowrap' : '')};
 	}
 `;
 
-export const Desc = styled.p<ExistingUserProp>`
+export const Desc = styled.p`
 	color: ${colors.mainTitle};
 	margin-bottom: 32px;
 	font-weight: ${fonts.title.fontWeight.tablet};
 	font-size: ${fonts.title.fontSize.tablet}px;
 	line-height: ${fonts.title.lineHeight};
-	letter-spacing: ${fonts.title.letterSpacing.tablet}px;
 	@media screen and (${media.tablet}) {
 		margin-bottom: 24px;
 	}
 	@media screen and (${media.desktop}) {
 		margin: 0 auto 32px;
-		max-width: ${(p) => (p.existingUser ? '320px' : '')};
 	}
 `;
 
-export const Reminder = styled.h2<ExistingUserProp>`
+export const Link = styled.a`
+	color: ${colors.secondaryAccent};
+	text-decoration: underline;
+`;
+
+export const Reminder = styled.h2`
 	color: ${colors.mainTitle};
 	font-weight: ${fonts.title.fontWeight.tablet};
 	font-size: ${fonts.title.fontSize.tablet}px;
@@ -80,7 +78,6 @@ export const Reminder = styled.h2<ExistingUserProp>`
 		line-height: ${fonts.headline.lineHeight.tablet};
 		font-weight: ${fonts.headline.fontWeight.tablet};
 		margin: 0 auto;
-		max-width: ${(p) => (p.existingUser ? '320px' : '')};
 	}
 `;
 
--- client/components/EndPopUp/EndPopUp.tsx
+++ client/components/EndPopUp/EndPopUp.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Desc, Img, Reminder, Section, Span, Title } from './EndPopUp.styles';
+import { Desc, Img, Link, Reminder, Section, Span, Title } from './EndPopUp.styles';
 
 export function EndPopUp({ existingUser = false }: { existingUser?: boolean }) {
-	return !existingUser ? (
+	return existingUser ? (
 		<Section>
 			<Title>Дякуємо!</Title>
 			<Desc>Твоя анкета буде оброблена протягом доби</Desc>
@@ -16,11 +16,13 @@ export function EndPopUp({ existingUser = false }: { existingUser?: boolean }) {
 			</Reminder>
 		</Section>
 	) : (
-		<Section existingUser={existingUser}>
-			<Title existingUser={existingUser}>Поштова скринька вже зареєстрована!</Title>
-			<Desc existingUser={existingUser}>Якщо це не твоя електронна пошта - заповни анкету повторно</Desc>
-			<Reminder existingUser={existingUser}>
-				Перевір запрошення до{' '}
+		<Section>
+			<Title>Поштова скринька вже зареєстрована!</Title>
+			<Desc>
+				Якщо це не твоя електронна пошта - <br /> <Link>заповни анкету повторно</Link>
+			</Desc>
+			<Reminder>
+				Перевір запрошення <br /> до{' '}
 				<Span>
 					<Img src={'/discord.svg'} alt="Discord icon" width="" height="" />
 					Discord
