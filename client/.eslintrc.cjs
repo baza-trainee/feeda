c494e2bc11f995c412c4d098900b0769b45099ba
Maksym Parunov <parunovmaksym@gmail.com>
2023-07-19 11:41:24 +0300

    config: eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,42 +1,41 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/jsx-runtime',
-    'next/core-web-vitals',
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
-  rules: {
-    indent: ['error', ['tab', 'spaces']],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'always'],
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          ['^react'],
-          ['^@?\\w'],
-          ['@/(.*)'],
-          ['^~'],
-          ['^[./]'],
-          ['^~assets'],
-          ['@.+.(sc|sa|c)ss$'],
-          ['.(sc|sa|c)ss$'],
-          ['.module.(sc|sa|c)ss$'],
-        ],
-      },
-    ],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/jsx-runtime',
+		'next/core-web-vitals',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+	rules: {
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		semi: ['error', 'always'],
+		'simple-import-sort/imports': [
+			'error',
+			{
+				groups: [
+					['^react'],
+					['^@?\\w'],
+					['@/(.*)'],
+					['^~'],
+					['^[./]'],
+					['^~assets'],
+					['@.+.(sc|sa|c)ss$'],
+					['.(sc|sa|c)ss$'],
+					['.module.(sc|sa|c)ss$'],
+				],
+			},
+		],
+	},
 };
