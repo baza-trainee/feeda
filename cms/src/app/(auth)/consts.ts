ecb819e4d50f1157fae3cae924bb3b8aeb253a44
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-07-22 15:14:01 +0300

    feat: add consts with text to labels, titles and placeholders

---
+++ cms/src/app/(auth)/consts.ts
@@ -0,0 +1,25 @@
+export const formTitle = {
+	login: 'Вхід',
+	'recover-password': 'Відновити пароль',
+};
+
+export const labelsTitle = {
+	login: 'Логін',
+	password: 'Пароль',
+	mail: 'E-mail',
+	'new-password': 'Новий пароль*',
+	'repeat-new-password': 'Підтвердити новий пароль*',
+};
+
+export const inputPlaceholderText = {
+	login: 'Логін',
+	password: 'Пароль',
+	mail: 'E-mail',
+};
+
+export const btnText = {
+	login: 'Увійти',
+	password: 'Пароль',
+	mail: 'E-mail',
+	send: 'Надіслати',
+};
