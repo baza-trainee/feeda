0573aef9e638d3d2ab1dccc1800665b028a0b91f
Maksym Parunov <parunovmaksym@gmail.com>
2023-07-31 13:24:13 +0300

    refactor: linting

--- cms/.eslintrc.cjs
+++ cms/.eslintrc.cjs
@@ -18,7 +18,6 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
   rules: {
-    indent: ['error', 'tab'],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
