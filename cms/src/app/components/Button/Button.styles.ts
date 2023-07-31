c1a41848ef33bb07686b61a5e811879689277244
maxkulbako <mkulbako@gmail.com>
2023-07-31 15:49:20 +0200

    feature: add text btn

--- cms/src/app/components/Button/Button.styles.ts
+++ cms/src/app/components/Button/Button.styles.ts
@@ -18,16 +18,36 @@ export const PrimaryBtn = styled.button<{ isPressed: boolean; disabled: boolean 
     cursor: default;
   }
 
-  &:not(:disabled):hover,
-  :not(:disabled):focus {
+  &:not(:disabled):hover {
     color: #ffbd00;
     background-color: #232323;
   }
 
-  ${({ isPressed }) =>
-    isPressed
-      ? ' background-color: #ffbd00 !important; border: 1px solid #ffbd00 !important; color: #232323 !important;'
-      : ''}
+  ${({ isPressed }) => (isPressed ? ' background-color: #ffbd00 !important; color: #232323 !important;' : '')}
+
+  transition: all 250ms ease-in;
+`;
+
+export const TextBtn = styled(PrimaryBtn)`
+  padding: 16px;
+  font-weight: 600;
+  font-size: 16px;
+  border: 1px solid #232323;
+  color: #232323;
+  background-color: #fff;
+
+  &:disabled {
+    color: #bfbfbf;
+    background-color: #fcfcfc;
+    border-color: #bfbfbf;
+  }
+
+  &:not(:disabled):hover {
+    background-color: #fde8af;
+    color: #232323;
+  }
+
+  ${({ isPressed }) => (isPressed ? ' background-color: #232323 !important; color: #FDF5DD !important;' : '')}
 
   transition: all 250ms ease-in;
 `;
--- cms/src/app/components/Button/Button.tsx
+++ cms/src/app/components/Button/Button.tsx
@@ -1,31 +1,36 @@
 'use client';
 /** @jsxImportSource @emotion/react */
 import { ReactNode, useState } from 'react';
-import { PrimaryBtn } from './Button.styles';
+import { PrimaryBtn, TextBtn } from './Button.styles';
+import Image from 'next/image';
 
 type ButtonProps = {
   children?: ReactNode | string;
   isDisabled?: boolean;
   func?: () => void;
-  btnType: 'button' | 'submit';
+  btnType?: 'button' | 'submit';
+  variant: 'primary' | 'text';
 };
 
-export const Button = ({ children, isDisabled, func, btnType }: ButtonProps) => {
+export const Button = ({ children, isDisabled, func, variant, btnType = 'button' }: ButtonProps) => {
   const [isPressed, setIsPressed] = useState(false);
 
   const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (func) func();
   };
 
+  const ButtonComponent = variant === 'text' ? TextBtn : PrimaryBtn;
+
   return (
-    <PrimaryBtn
+    <ButtonComponent
       onClick={onClickHandler}
       onMouseDown={() => setIsPressed(true)}
       onMouseUp={() => setIsPressed(false)}
       disabled={isDisabled || false}
       isPressed={isPressed}
+      type={btnType}
     >
       {children}
-    </PrimaryBtn>
+    </ButtonComponent>
   );
 };
