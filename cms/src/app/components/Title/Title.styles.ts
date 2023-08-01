dd9ac2b177a9d6e64fb74a1abd95f96c743b7705
maxkulbako <mkulbako@gmail.com>
2023-08-01 07:34:15 +0200

    add title component

---
+++ cms/src/app/components/Title/Title.styles.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const MainTitle = styled.h1`
+  font-weight: 600;
+  font-size: 32px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 45px;
+    font-weight: 700;
+  }
+`;
+
+export const SecondaryTitle = styled.h1`
+  font-weight: 600;
+  font-size: 28px;
+
+  @media screen and (min-width: 1280px) {
+    font-size: 32px;
+  }
+`;
