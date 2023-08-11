561213eb23c5b66d6115d32c56af708b8d03c05b
Nazar <83466233+tipanazar@users.noreply.github.com>
2023-08-11 20:41:06 +0100

    Update regexs.ts

--- cms/src/hooks/regexs.ts
+++ cms/src/hooks/regexs.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-useless-escape */
-export const nameRegex = /^[-'A-Za-zA-Яа-яЄєІіЇїҐґ]+$/;
+export const nameRegex = /^[A-Za-zA-Яа-яЄєІіЇїҐґ\-'\s]+$/;
 export const phoneNumberRegex = /^\+\d{12}$/;
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const discordRegex = /^(?:[A-Za-z]+#(\d{4})|[a-z0-9_.]*[a-z][a-z0-9_.]*)$/i;
