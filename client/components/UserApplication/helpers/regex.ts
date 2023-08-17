77247daf94422e9230d2966aa7145e640ec7f26c
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-08-17 22:31:58 +0300

    fix: * linkedin regex for bug_4.7.1 requirements

--- client/components/UserApplication/helpers/regex.ts
+++ client/components/UserApplication/helpers/regex.ts
@@ -7,6 +7,6 @@ export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export const discordRegex = /^(?:[A-Za-z]+#(\d{4})|[a-z0-9_.]*[a-z][a-z0-9_.]*)$/i;
 
-export const linkedRegex = /^https:\/\/www\.linkedin\.com\/in\/[A-Za-zА-Яа-я0-9\-.:\/]+$/;
+export const linkedRegex = /^(?:https?:\/\/(?:www\.)?linkedin\.com\/in\/)?[A-Za-zА-Яа-я0-9\-.:\/]+$/;
 
 export const cityRegex = /^[A-Za-zА-Яа-яЄєІіЇїҐґ\-',()\s]+$/;
