9b2bbd98bbb21674aec5c9a1e5edbe42bc2b253e
maxkulbako <mkulbako@gmail.com>
2023-08-23 11:54:13 +0200

    fix: button and project page styles

--- cms/src/app/projects/ProjectsPage.styles.ts
+++ cms/src/app/projects/ProjectsPage.styles.ts
@@ -5,18 +5,7 @@ import { theme } from '../../styles/theme';
 export const ProjectsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  //max-width: 335px;
   gap: 24px;
-
-  @media screen and (${theme.media.tablet}) {
-    margin-left: 240px; // temporaty
-    //max-width: 464px;
-  }
-
-  @media screen and (${theme.media.desktop}) {
-    margin-left: 300px; // temporaty
-    //max-width: 906px;
-  }
 `;
 
 export const AddButtonWrapper = styled.div`
