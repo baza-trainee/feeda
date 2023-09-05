9ccd196bf9bca5c8cf6128b3728feddf7328ca8d
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-09-05 21:19:16 +0300

    refresh

--- cms/src/app/(auth)/new-password/NewPass.tsx
+++ cms/src/app/(auth)/new-password/NewPass.tsx
@@ -8,55 +8,60 @@ import { Input } from '~/src/components/Input/Input';
 import { Title } from '~/src/components/Title/Title';
 import { AppDispatch } from '~/src/redux/store/store';
 
+import { setNewPassword } from '../authOperations/operations';
 import { CheckboxComponent } from '../components/CheckboxComponent/CheckboxComponent';
 import { btnText, formTitle, inputPlaceholderText, labelsTitle, patternsCheck } from '../consts';
 import { NewPassFormCss } from './NewPass.styles';
 
 export function NewPassForm() {
-	const { control, clearErrors, getValues } = useForm();
+  const { control, clearErrors, getValues } = useForm();
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		console.log(getValues());
-	};
+  const dispatch = useDispatch<AppDispatch>();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { newPassword, repeatNewPassword } = getValues();
+    const data = { newPassword, repeatNewPassword };
+    dispatch(setNewPassword(data));
+    console.log(getValues());
+  };
 
-	return (
-		<form onSubmit={handleSubmit} css={NewPassFormCss}>
-			<div css={TitleCss}>
-				<Title title={formTitle['recover-password']} main />
-			</div>
-			<div>
-				<div css={ContainerCss}>
-					<Input
-						placeholder={inputPlaceholderText.password}
-						type="password"
-						name="newPassword"
-						id="newPassword"
-						control={control}
-						clearErrors={clearErrors}
-						label={labelsTitle['new-password']}
-						pattern={patternsCheck.login.source}
-						minLength={8}
-						maxLength={12}
-					/>
-				</div>
-				<div css={ContainerCss}>
-					<Input
-						placeholder={inputPlaceholderText.password}
-						type="password"
-						name="repeatNewPassword"
-						id="repeatNewPassword"
-						control={control}
-						clearErrors={clearErrors}
-						label={labelsTitle['repeat-new-password']}
-						pattern={patternsCheck.login.source}
-						minLength={8}
-						maxLength={12}
-					/>
-				</div>
-			</div>
-			<CheckboxComponent />
-			<Button btnType="submit" title={btnText.login} variant="primary"></Button>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit} css={NewPassFormCss}>
+      <div css={TitleCss}>
+        <Title title={formTitle['recover-password']} main />
+      </div>
+      <div>
+        <div css={ContainerCss}>
+          <Input
+            placeholder={inputPlaceholderText.password}
+            type="password"
+            name="newPassword"
+            id="newPassword"
+            control={control}
+            clearErrors={clearErrors}
+            label={labelsTitle['new-password']}
+            pattern={patternsCheck.login.source}
+            minLength={8}
+            maxLength={12}
+          />
+        </div>
+        <div css={ContainerCss}>
+          <Input
+            placeholder={inputPlaceholderText.password}
+            type="password"
+            name="repeatNewPassword"
+            id="repeatNewPassword"
+            control={control}
+            clearErrors={clearErrors}
+            label={labelsTitle['repeat-new-password']}
+            pattern={patternsCheck.login.source}
+            minLength={8}
+            maxLength={12}
+          />
+        </div>
+      </div>
+      <CheckboxComponent />
+      <Button btnType="submit" title={btnText.login} variant="primary"></Button>
+    </form>
+  );
 }
