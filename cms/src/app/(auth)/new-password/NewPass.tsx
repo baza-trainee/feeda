77bdb7e3b679b46a3becb0be7e95ce22cd540767
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-09-05 22:11:46 +0300

    feature/add functional reset password

--- cms/src/app/(auth)/new-password/NewPass.tsx
+++ cms/src/app/(auth)/new-password/NewPass.tsx
@@ -1,7 +1,10 @@
 'use client';
 /** @jsxImportSource @emotion/react */
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+
+import { useRouter } from 'next/navigation';
 
 import { Button } from '~/src/components/Button/Button';
 import { Input } from '~/src/components/Input/Input';
@@ -16,22 +19,27 @@ import { NewPassFormCss } from './NewPass.styles';
 export function NewPassForm() {
   const { control, clearErrors, getValues } = useForm();
 
+  const router = useRouter();
+
   const dispatch = useDispatch<AppDispatch>();
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const { newPassword, repeatNewPassword } = getValues();
     const data = { newPassword, repeatNewPassword };
     dispatch(setNewPassword(data));
-    console.log(getValues());
+    console.log(data);
+    if (newPassword === repeatNewPassword) {
+      router.push('/login');
+    }
   };
 
   return (
     <form onSubmit={handleSubmit} css={NewPassFormCss}>
-      <div css={TitleCss}>
+      <div>
         <Title title={formTitle['recover-password']} main />
       </div>
       <div>
-        <div css={ContainerCss}>
+        <div>
           <Input
             placeholder={inputPlaceholderText.password}
             type="password"
@@ -40,12 +48,12 @@ export function NewPassForm() {
             control={control}
             clearErrors={clearErrors}
             label={labelsTitle['new-password']}
-            pattern={patternsCheck.login.source}
+            // pattern={patternsCheck.login.source}
             minLength={8}
             maxLength={12}
           />
         </div>
-        <div css={ContainerCss}>
+        <div>
           <Input
             placeholder={inputPlaceholderText.password}
             type="password"
@@ -54,7 +62,7 @@ export function NewPassForm() {
             control={control}
             clearErrors={clearErrors}
             label={labelsTitle['repeat-new-password']}
-            pattern={patternsCheck.login.source}
+            // pattern={patternsCheck.login.source}
             minLength={8}
             maxLength={12}
           />
